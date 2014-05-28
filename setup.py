199b169e355a0b6163c51c5acf420207401d2159
Martin Froehlich <martin.j.froehlich@googlemail.com>
2014-05-28 11:11:57 +0200

    bump version number to Major.minor.revision.3

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     url='http://github.com/jfinkels/flask-restless',
-    version='1.14.0.2-dev',
+    version='1.14.0.3-dev',
     zip_safe=False
 )
