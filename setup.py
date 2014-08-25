74037702affe6ab4821f82642d0304e824f28213
Johannes Steger <jss@coders.de>
2014-08-25 12:52:39 +0200

    sanitize order_by parameters

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='nose.collector',
     tests_require=['nose'],
     url='http://github.com/jfinkels/flask-restless',
-    version='1.14.0.9-dev',
+    version='1.14.0.10-dev',
     zip_safe=False
 )
