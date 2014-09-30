a4ce15f0c9b9a4001738a7fcc06cfc71673fc4b6
Giampaolo Rodola <g.rodola@gmail.com>
2014-09-30 15:46:04 +0200

    bump up version number

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     tests_require=['nose'],
     extras_require={'xml': ["lxml"]},
     url='http://github.com/jfinkels/flask-restless',
-    version='1.14.0.11-dev',
+    version='1.14.0.12-dev',
     zip_safe=False
 )
